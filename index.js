b104ba5876c3faf9545ab573a5289becae270882
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-03-02 13:22:32 +0300

    Adds an even listener to a button

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    function clickAlert() {
+        alert('Check your inbox!')
+    }
+    
+    input.addEventListener('click', clickAlert);
 }
+
+
+// const input = document.getElementById('button');
+// function clickAlert() {
+//     alert('Check your inbox!')
+// }
+
+// input.addEventListener('click', clickAlert);
