17647b7549f2bfdbb5f97626b161fabecfa46ddf
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-03-13 21:47:21 +0300

    Adds event listener to the button in index.js and commented out other ways of doing the same task

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ function addingEventListener() {
     function clickAlert() {
         alert('Check your inbox!')
     }
-    
     input.addEventListener('click', clickAlert);
 }
 
+addingEventListener()
 
-// const input = document.getElementById('button');
+// const input = document.getElementById("button");
 // function clickAlert() {
-//     alert('Check your inbox!')
+//   alert("Check your inbox!");
 // }
 
-// input.addEventListener('click', clickAlert);
+// input.addEventListener("click", clickAlert);
+
+// const input = document.getElementById("button");
+// input.addEventListener("click", function () {
+//   alert("I was clicked");
+// });
